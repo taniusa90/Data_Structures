c11576ac0c755ddf1cb78af01d5a755a520d480d
taniusa90 <galea_tanea@mail.com>
2022-10-14 11:58:57 -0500

    Queue

---
+++ Queues/src/main/java/CollectionsQueue.java
@@ -0,0 +1,10 @@
+public class CollectionsQueue {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
---
+++ Queues/src/main/java/MyQueue.java
@@ -0,0 +1,49 @@
+import java.util.NoSuchElementException;
+
+public class MyQueue <T>{
+
+    QNode<T> front;
+    QNode<T> back;
+    int size;
+    boolean isEmpty(){
+        return front==null;
+    }
+
+    T peek(){
+        return (T)front.value;
+    }
+
+    void enqueue(T item){//add the item
+
+        QNode<T> node = new QNode<T>(item);
+        if (isEmpty()){
+            front=back=node;
+        }else {
+            back.next=node;
+            back=node;
+
+        }
+        size++;
+    }
+
+    T dequeue(){//remove te item
+        QNode<T> frontNode;
+        if (isEmpty()){
+            throw new NoSuchElementException();
+        }
+        if (front==back){
+            frontNode=front;
+            front=back=null;
+        }else {
+            frontNode=front;
+            front=front.next;
+        }
+        size--;
+        return (T) frontNode.value;
+    }
+    int size(){
+        return size;
+
+
+    }
+}
