82c31cbc9e5589bad2fc5f5f9411834f03591964
taniusa90 <galea_tanea@mail.com>
2022-11-10 08:47:37 -0600

    Tree

--- Trees/src/main/java/MyTree.java
+++ Trees/src/main/java/MyTree.java
@@ -35,62 +35,77 @@ public class MyTree {
             }
         }
     }
-
-    //PreOrder Traversal of the tree
-    //Root-Left-Right
+    // PreOrder Traversal of the tree
+    // Root-Left-right
     void preOrderTraversal(TNode root){
-        if (root==null){
-            return;
-        }
-        System.out.print(root.value+", ");
-        preOrderTraversal(root.leftChild);
-        preOrderTraversal(root.rightChild);
-
-
-
-
-        }
-
-    //InOrder
-    //Left-Root-Right
-        void inOrderTraversal(TNode root){
-            if (root==null){
-                return;
-            }
-            inOrderTraversal(root.leftChild);
-            System.out.print(root.value+", ");
-            inOrderTraversal(root.rightChild);
+        if(root==null) return;// termination
+        System.out.print(root.value +", "); // visit root
+        preOrderTraversal(root.leftChild);  // visit left subtree
+        preOrderTraversal(root.rightChild); // visit right subtree
     }
-    //Post-Order
-    //Left-Right-Root
-    void postOrder(TNode root){
-        if (root==null){
-            return;
-        }
-        postOrder(root.leftChild);
-        postOrder(root.rightChild);
+    void inOrderTraversal(TNode root){
+        if (root==null) return; // termination
+        inOrderTraversal(root.leftChild);
         System.out.print(root.value+", ");
+        inOrderTraversal(root.rightChild);
     }
-
+    void postOrderTraversal(TNode root){
+        if (root==null) return;// termination
+        postOrderTraversal(root.leftChild); // branch and finish left subtree
+        postOrderTraversal(root.rightChild); // branch and finish right subtree
+        System.out.print(root.value+", ");  // visit root
+    }
     void levelOrderTraversal(){
-        if (root==null){
-            return;
-        }
-        Queue<TNode>queue=new LinkedList<>();
-        queue.add(root);//we add the root to queue
-        while (!queue.isEmpty()){
-            TNode toVisit=queue.poll();//front element of the queue
+        if(root==null) return;
+        Queue<TNode>  queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TNode toVisit=queue.poll();
             System.out.print(toVisit.value+", ");
-            if (toVisit.leftChild!=null){//i have a left child
-                queue.add(toVisit.leftChild);
-
-            }
-            if (toVisit.rightChild!=null){
-                queue.add(toVisit.rightChild);
-            }
+            if (toVisit.leftChild!=null) queue.add(toVisit.leftChild);
+            if (toVisit.rightChild!=null) queue.add(toVisit.rightChild);
         }
     }
+    public boolean contains(int value){
+        if (root==null) return false;
+        TNode current= root;
+        while (current!=null) {
+            if (value<current.value) current=current.leftChild;
+            else if (value> current.value) current=current.rightChild;
+            else return true;
+        }
+        return false;
+    }
+    public boolean isLeaf(TNode node) {
+        return node.leftChild==null && node.rightChild==null;
+    }
+    public void printLeaves(TNode root){
+        if (root==null) return;
+        // perform visit on Root
 
+        // Recursively Branch Left Subtree
+        printLeaves(root.leftChild);
+        printLeaves(root.rightChild);
+        if (isLeaf(root)) System.out.print(root.value + ", ");
 
-
+        // Recursively Branch Right Subtree
+    }
+    int countLeaves(TNode root){
+        if (root==null) return 0;
+        if (isLeaf(root)) return 1;
+        // recursively left
+        //  right
+        return countLeaves(root.leftChild) + countLeaves(root.rightChild);
+    }
+    int findSumOfLeaves(TNode root){
+        if (root==null) return 0;
+        if (isLeaf(root)) return root.value;
+        return findSumOfLeaves(root.leftChild) + findSumOfLeaves(root.rightChild);
+    }
+    int height(TNode root){
+        if (root==null) return -1;
+        if (isLeaf(root)) return 0;
+        return 1+ Math.max(height(root.leftChild), height(root.rightChild));
+    }
 }
+
